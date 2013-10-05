6601dc28538eb9f939b9be2ee6c3e0f1444b5d0a
Ernesto Migliore <migliore@to.infn.it>
2013-10-05 06:28:30 +0200

    MuonProducer to be used for 53X re-reco of the muons

--- AuxCode/AuxMuonSelectors/python/tightmuonproducer_cfi.py
+++ AuxCode/AuxMuonSelectors/python/tightmuonproducer_cfi.py
@@ -2,5 +2,6 @@ import FWCore.ParameterSet.Config as cms
 
 demo = cms.EDProducer('TightMuonProducer',
                       muonSrc=cms.InputTag('muons'),
-                      vertexSrc=cms.InputTag('offlinePrimaryVertices')
+                      vertexSrc=cms.InputTag('offlinePrimaryVertices'),
+                      isPF=cms.bool(True)
 )
