7088dde92b953fbd52dfd247287f0f6d9ab44ed8
Ernesto Migliore <migliore@to.infn.it>
2013-10-22 13:20:40 +0200

    revert to usercode/part 1

--- FWCore/Framework/src/WorkerT.cc
+++
@@ -1,8 +0,0 @@
-#include "FWCore/Framework/src/WorkerT.h"
-#include "FWCore/Framework/interface/EventPrincipal.h"
-
-namespace edm{
-  UnscheduledHandler* getUnscheduledHandler(EventPrincipal const& ep) {
-    return ep.unscheduledHandler().get();
-  }
-}
