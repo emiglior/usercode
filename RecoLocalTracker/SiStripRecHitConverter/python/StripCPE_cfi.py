3dffce618ba895560c74cefc55c3f67c89819d9b
Ernesto Migliore <migliore@to.infn.it>
2013-10-22 13:41:54 +0200

    revert to usercode/part 2

--- RecoLocalTracker/SiStripRecHitConverter/python/StripCPE_cfi.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-StripCPEESProducer = cms.ESProducer("StripCPEESProducer",
-                                    ComponentName = cms.string('SimpleStripCPE'),
-                                    ComponentType = cms.string('SimpleStripCPE')
-)
-
-
--- RecoLocalTracker/SiStripRecHitConverter/python/StripCPEfromTrackAngle_cfi.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-StripCPEfromTrackAngleESProducer = cms.ESProducer("StripCPEESProducer",
-                                                  ComponentName = cms.string('StripCPEfromTrackAngle'),
-                                                  ComponentType = cms.string('StripCPEfromTrackAngle')
-                                                  )
-
-
