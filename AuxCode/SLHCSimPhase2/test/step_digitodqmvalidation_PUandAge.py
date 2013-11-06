6f0a61ad474101593c3dcf98039156ccb955ba1b
Ernesto Migliore <migliore@to.infn.it>
2013-11-06 14:27:45 +0100

    second test of changing the pixel size (including CPE) /2

--- AuxCode/SLHCSimPhase2/test/step_digitodqmvalidation_PUandAge.py
+++ AuxCode/SLHCSimPhase2/test/step_digitodqmvalidation_PUandAge.py
@@ -260,7 +260,7 @@ process = customise_tkonly(process)
 # TODO 1) the choice of the array shotld be steerable from somewhere
 #      2) the next lines could be fed into the process via the customize option of the cmsDriver.py 
 from AuxCode.SLHCSimPhase2.PixelCPE_tables_cff import *
-process.PixelCPEGenericESProducer.PixelCPEList = options.pixel_CPE
+process.PixelCPEGenericESProducer.PixelCPEList = options.PixelCPE
 
 # Uncomment next two lines to change pixel DIGI threshold
 process.mix.digitizers.pixel.ThresholdInElectrons_BPix = cms.double(options.BPixThr)
