ed5ba0cea389a49d6fd08053a94f3e61527faa33
Marco Musich <marco.musich@cern.ch>
2014-07-18 10:12:28 +0200

    adding position of the sim/(rec)Hit to the canvas

--- AuxCode/SLHCSimPhase2/test/macros/DumpPixelTree.py
+++ AuxCode/SLHCSimPhase2/test/macros/DumpPixelTree.py
@@ -31,6 +31,8 @@ def declare_struct():
     Float_t         yy;\
     Float_t         row;\
     Float_t         col;\
+    Float_t         hrow;\
+    Float_t         hcol;\
     Float_t         gx;\
     Float_t         gy;\
     Float_t         gz;\
@@ -45,6 +47,10 @@ def declare_struct():
     Int_t           nsimhit;\
     Int_t           spreadx;\
     Int_t           spready;\
+    Int_t           nRowsInDet;\
+    Int_t           nColsInDet;\
+    Float_t         pitchx;\
+    Float_t         pitchy;\
     Float_t         hx;\
     Float_t         hy;\
     Float_t         tx;\
@@ -71,10 +77,12 @@ def main():
     parser.add_option("-e", "--evts-to-dump",
                       action="store", type="int", dest="evts_to_dump", default=20,
                       help="max number of events to dump (for event display)")
+    parser.add_option("-v", "--verbose",
+                      action="store_true", dest="verbose", default=False,
+                      help="verbose output")
+
     (options, args) = parser.parse_args()
 
-
-    verbose = False
     max_evt_dumped = options.evts_to_dump
     max_evt_dumped+=1 # temporary fix.... should find a better way to dump "last" event
 
@@ -90,7 +98,7 @@ def main():
         sys.exit()
 
     input_tree = input_root_file.Get("PixelNtuple")            
-    if verbose: input_tree.Print()
+    if options.verbose: input_tree.Print()
 
 
     # sort the tree in case you want to display more than 1 cluster on the same module
@@ -107,7 +115,7 @@ def main():
     for i in range(nentries):
         uniqueId.append(int(input_tree.GetV1()[i]))
     index_uniqueId = argsort(uniqueId)
-    if verbose:
+    if options.verbose:
         for i in range(nentries):
             print i, index_uniqueId[i]
 
@@ -127,8 +135,11 @@ def main():
     nR = 160  # rows or local_X
     nC = 8*52 # cols or local_Y
     h2_localXY_digi = []
+    theRecHitPoints = []
+    theSimHitPoints = []
     c_localXY_digi = []
     evt_dumped = 0
+    recHitCount = 1
     uniqueId_old = -1
 
 #    print len(h2_localXY_digi)
@@ -136,7 +147,7 @@ def main():
     for this_entry in xrange(all_entries):        
 
         if this_entry % 100 == 0:
-            print "Procesing Event: ", this_entry
+            print "Processing rechit: ", this_entry
 
         input_tree.GetEntry(index_uniqueId[this_entry])
 
@@ -151,6 +162,8 @@ def main():
         pixel_recHit.yy         = input_tree.yy
         pixel_recHit.row        = input_tree.row
         pixel_recHit.col        = input_tree.col
+        pixel_recHit.hrow       = input_tree.hrow
+        pixel_recHit.hcol       = input_tree.hcol
         pixel_recHit.gx         = input_tree.gx
         pixel_recHit.gy         = input_tree.gy
         pixel_recHit.gz         = input_tree.gz
@@ -165,6 +178,10 @@ def main():
         pixel_recHit.nsimhit    = input_tree.nsimhit
         pixel_recHit.spreadx    = input_tree.spreadx
         pixel_recHit.spready    = input_tree.spready
+        pixel_recHit.pitchx     = input_tree.pitchx
+        pixel_recHit.pitchy     = input_tree.pitchy
+        pixel_recHit.nColsInDet = input_tree.nColsInDet
+        pixel_recHit.nRowsInDet = input_tree.nRowsInDet
         pixel_recHit.hx         = input_tree.hx
         pixel_recHit.hy         = input_tree.hy
         pixel_recHit.tx         = input_tree.tx
@@ -190,29 +207,60 @@ def main():
         # BPIX layer 1 only 
         if pixel_recHit.subid==1 and pixel_recHit.layer==1:     
 
-            if verbose: print  "spread X, spread Y, DgN", pixel_recHit.spreadx, pixel_recHit.spready, pixel_recHit.DgN
+            if options.verbose: 
+                print  "spread X, spread Y, DgN", pixel_recHit.spreadx, pixel_recHit.spready, pixel_recHit.DgN
+                print  "nColsInDet: ",pixel_recHit.nColsInDet," nRowInDet: ",pixel_recHit.nRowsInDet," pitchx: ",pixel_recHit.pitchx," pitchy: ",pixel_recHit.pitchy         
             for iDg in range(pixel_recHit.DgN):
-                if verbose: print iDg, pixel_recHit.DgDetId[iDg], pixel_recHit.DgRow[iDg], pixel_recHit.DgCol[iDg]
+                if options.verbose: print iDg, pixel_recHit.DgDetId[iDg], pixel_recHit.DgRow[iDg], pixel_recHit.DgCol[iDg]
                 print >> output_ascii_file, evt.evtnum,  pixel_recHit.DgDetId[iDg], pixel_recHit.DgRow[iDg], pixel_recHit.DgCol[iDg], int(pixel_recHit.DgCharge[iDg]*1000) # ADC (in ke) converted to int
             print >> output_ascii_file, "--> next rechit"
 
             # straw man event display
             if evt_dumped < max_evt_dumped:
+
                 if evt.evtnum*1000000000+pixel_recHit.DgDetId[0] > uniqueId_old: 
 
                     if evt_dumped > 0:
                         c_localXY_digi[-1].cd()
                         h2_localXY_digi[-1].SetTitle("Evt: "+str(uniqueId_old/1000000000)+" Mod: "+str(uniqueId_old % 1000000000))
+
                         if options.lego:
                             h2_localXY_digi[-1].Draw("LEGOcolz")
                         else:
                             h2_localXY_digi[-1].Draw("colz")
 
+                        theRecHitPoints[-1].SetMarkerStyle(20)    
+                        theSimHitPoints[-1].SetMarkerStyle(3)
+
+                        theRecHitPoints[-1].SetMarkerSize(1.)
+                        theSimHitPoints[-1].SetMarkerSize(1.)
+                        
+                        theRecHitPoints[-1].SetMarkerColor(ROOT.kBlack)
+                        theSimHitPoints[-1].SetMarkerColor(ROOT.kRed)
+                        
+                        #print "theRecHitPoints[-1].GetN() :", theRecHitPoints[-1].GetN(), "recHitCount :", recHitCount
+
+                        theRecHitPoints[-1].Set(recHitCount)
+                        theSimHitPoints[-1].Set(recHitCount)
+                        theRecHitPoints[-1].RemovePoint(0)
+                        theSimHitPoints[-1].RemovePoint(0)
+
+                        #print "theRecHitPoints[-1].GetN() :", theRecHitPoints[-1].GetN()
+
+                        theRecHitPoints[-1].Draw("Psame")
+                        theSimHitPoints[-1].Draw("Psame")
+
                         h2_localXY_digi[-1].SetMaximum(40.)
                         c_localXY_digi[-1].SaveAs("c_localXY_digi_"+str(evt_dumped)+".png")
+                        #c_localXY_digi[-1].SaveAs("c_localXY_digi_"+str(evt_dumped)+".root")
+
+                        recHitCount=1
 
                     h2_localXY_digi.append(ROOT.TH2F("h2_localXY_digi"+str(evt_dumped),"h2_localXY_digi",nR,-0.5,-0.5+nR,nC,-0.5,-0.5+nC))
                     h2_localXY_digi[-1].SetStats(ROOT.kFALSE)  
+
+                    theRecHitPoints.append(ROOT.TGraph(100))
+                    theSimHitPoints.append(ROOT.TGraph(100))
 
                     if options.lego:
                         c_localXY_digi.append(ROOT.TCanvas("c_localXY_digi"+str(evt_dumped),"c_localXY_digi"))
@@ -223,8 +271,12 @@ def main():
                     evt_dumped += 1
 
                 for iDg in range(pixel_recHit.DgN):
-                    h2_localXY_digi[-1].SetBinContent(pixel_recHit.DgRow[iDg], pixel_recHit.DgCol[iDg], pixel_recHit.DgRow[iDg], pixel_recHit.DgCharge[iDg])
+                    h2_localXY_digi[-1].SetBinContent(pixel_recHit.DgRow[iDg], pixel_recHit.DgCol[iDg],pixel_recHit.DgCharge[iDg])
          
+                theRecHitPoints[-1].SetPoint(recHitCount,pixel_recHit.row,pixel_recHit.col)
+                theSimHitPoints[-1].SetPoint(recHitCount,pixel_recHit.hrow,pixel_recHit.hcol) 
+                recHitCount +=1
+
 # close ascii output
     output_ascii_file.close()
 
