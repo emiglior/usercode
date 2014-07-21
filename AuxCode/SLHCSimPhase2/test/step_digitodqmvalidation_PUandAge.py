13c36599848024a57ae831d5adf113c357e63116
Ernesto Migliore <migliore@to.infn.it>
2014-07-21 23:51:49 +0200

    Fix :: in path of MinBias GEN-SIM for PU events

--- AuxCode/SLHCSimPhase2/test/step_digitodqmvalidation_PUandAge.py
+++ AuxCode/SLHCSimPhase2/test/step_digitodqmvalidation_PUandAge.py
@@ -78,7 +78,7 @@ if options.PUScenario!="NoPU":
     # Used to be
     #process.load('SimGeneral.MixingModule.mix_E8TeV_AVE_16_BX_25ns_cfi')
     process.load('SimGeneral.MixingModule.mix_POISSON_average_cfi')
-    process.mix.input.fileNames = cms.untracked.vstring(['root:://eoscms//eos/cms/store/caf/user/emiglior/SLHCSimPhase2/620_slhc11/Extended2017/MinBias/step1_MinBias_TuneZ2star_14TeV_pythia6_12k_evts.root'])
+    process.mix.input.fileNames = cms.untracked.vstring(['root://eoscms//eos/cms/store/caf/user/emiglior/SLHCSimPhase2/620_slhc11/Extended2017/MinBias/step1_MinBias_TuneZ2star_14TeV_pythia6_12k_evts.root'])
     process.mix.bunchspace = cms.int32(25)
     process.mix.minBunch = cms.int32(-12)
     process.mix.maxBunch = cms.int32(3)
