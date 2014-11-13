6745b454b898b5f813ae9cbf88e1cd4dde377729
Ernesto Migliore <migliore@to.infn.it>
2014-11-13 06:14:54 +0100

    use 51k MinBias

--- AuxCode/SLHCSimPhase2/test/OneNuM_GEN_TO_RECO_PixelLocalRecoStudy_cfg.py
+++ AuxCode/SLHCSimPhase2/test/OneNuM_GEN_TO_RECO_PixelLocalRecoStudy_cfg.py
@@ -88,7 +88,7 @@ if options.PUScenario!="NoPU":
     # Used to be
     # process.load('SimGeneral.MixingModule.mix_E8TeV_AVE_16_BX_25ns_cfi')
     process.load('SimGeneral.MixingModule.mix_POISSON_average_cfi')
-    process.mix.input.fileNames = cms.untracked.vstring(['root://eoscms//eos/cms/store/caf/user/emiglior/SLHCSimPhase2/620_slhc17_patch1/Extended2023Muon/Thick_L0L1_0.285/step1_MinBias_TuneZ2star_14TeV_pythia6_3kEvts.root'])
+    process.mix.input.fileNames = cms.untracked.vstring(['root://eoscms//eos/cms/store/caf/user/emiglior/SLHCSimPhase2/620_slhc17_patch1/Extended2023Muon/GEN-SIM/Thick_BPIX_0.285_FPIX_0.285/step1_MinBias_TuneZ2star_14TeV_pythia6_51kEvts.root'])
     process.mix.bunchspace = cms.int32(25)
     process.mix.minBunch = cms.int32(-12)
     process.mix.maxBunch = cms.int32(3)
